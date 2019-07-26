d91a169fd169a0ba2bb4b09dcdd94f750f71b682
MykolaPavlenkov <50783365+MykolaPavlenkov@users.noreply.github.com>
2019-07-26 23:18:17 +0300

    'test'

---
+++ HomeWork/HomeWork1/task#3.py
@@ -0,0 +1,5 @@
+print ("""    
+    a. import this
+    b. import antigravity
+    c. import __hello__
+    d. from __future__ import braces""")
---
+++ HomeWork/HomeWork1/task#5.py
@@ -0,0 +1,8 @@
+print ("""Escape sequences
+\\a      Bell (alert)
+\\b      Backspace
+\\n      New line
+\\t      Horizontal tab
+\\\\     Backslash \\
+\”      Double quotation mark "
+\’      Single quotation mark ‘     """)
